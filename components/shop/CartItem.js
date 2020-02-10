08125a27b481a64d7708ea8839ddc4058f09fc70
Denizhan Gönen <denizhangonen@Denizhans-MacBook-Pro.local>
2020-02-11 00:44:27 +0300

    cart quantity display fix

--- components/shop/CartItem.js
+++ components/shop/CartItem.js
@@ -16,7 +16,7 @@ const CartItem = props => {
   return (
     <View style={styles.carItem}>
       <Text style={styles.itemData}>
-        <Text style={styles.quantity}>{props.quantity}</Text>
+        <Text style={styles.quantity}>({props.quantity})</Text>{" "}
         <Text style={styles.mainText}>{props.title}</Text>
       </Text>
       <View style={styles.itemData}>
